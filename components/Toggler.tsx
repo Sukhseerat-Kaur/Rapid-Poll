3c6ecf3042f90564a25d792564771bb4a5944c05
Sukhseerat Kaur <54837213+Sukhseerat-Kaur@users.noreply.github.com>
2022-03-15 18:23:10 +0530

    added toggler for auto expiring

--- components/Toggler.tsx
+++ components/Toggler.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 interface TogglerProps {
   label: string;
+  setShowDateTimeInput: Dispatch<SetStateAction<boolean>>;
 }
-const Toggler = ({ label }: TogglerProps) => {
+const Toggler = ({ label, setShowDateTimeInput }: TogglerProps) => {
   return (
     <div className="form-check form-switch">
       <div className="flex items-center justify-center w-full">
         <label htmlFor="toggleB" className="flex items-center cursor-pointer">
           <div className="mr-3 text-gray-600 font-medium">{label}</div>
           <div className="relative">
-            <input type="checkbox" id="toggleB" className="sr-only" />
+            <input
+              type="checkbox"
+              id="toggleB"
+              className="sr-only"
+              onChange={(e) => setShowDateTimeInput(e.target.checked)}
+            />
 
             <div className="block bg-gray-200 w-14 h-8 rounded-full transition duration-300"></div>
 
