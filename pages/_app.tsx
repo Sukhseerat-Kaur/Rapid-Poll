1bb04b4e278a64234dad55c530e9c475f08d3e11
Sukhseerat Kaur <54837213+Sukhseerat-Kaur@users.noreply.github.com>
2022-05-01 01:34:02 +0530

    added toast notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Toaster position="top-center" />
     </>
   );
 }
--- pages/new/[key].tsx
+++ pages/new/[key].tsx
@@ -2,14 +2,10 @@ import { useRouter } from "next/router";
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Header from "../../components/Header";
-import {
-  ArrowRight,
-  CopyIcon,
-  TwitterIcon,
-  WhatsAppIcon,
-} from "../../assets/Icons";
+import { ArrowRight, CopyIcon } from "../../assets/Icons";
 import ShareOnWhatsapp from "../../components/SocialButtons/ShareOnWhatsapp";
 import ShareOnTwitter from "../../components/SocialButtons/ShareOnTwitter";
+import toast from "react-hot-toast";
 
 interface Props {
   host: string | null;
@@ -25,10 +21,17 @@ const PollCreated = (props: Props) => {
   const KEY = router.query.key;
   const POLL_URL = `${BASE_PATH}/poll/${KEY}`;
 
-  const copyText = (text: string) => {
-    navigator.clipboard
-      .writeText(text)
-      .then((success) => console.log("text copied"));
+  const copyText = async (text: string) => {
+    const myPromise = navigator.clipboard.writeText(text);
+    toast.promise(
+      myPromise,
+      {
+        loading: "Copying...",
+        success: "Copied to Clipboard",
+        error: "Error Occurred While Copying",
+      },
+      { style: { minWidth: "250px" } }
+    );
   };
 
   return (
@@ -38,9 +41,12 @@ const PollCreated = (props: Props) => {
       </Head>
       <Header />
       <main className="flex-1 flex justify-center items-center shadow-sm flex-col gap-10">
+        <div className="text-3xl font-bold mb-14 text-red-600">
+          Your poll has been created successfully
+        </div>
         <div className="w-2/5 flex flex-col gap-5">
-          <div className="w-full h-max content shadow-md rounded-md bg-white p-4 flex flex-col justify-center gap-3">
-            <div className="font-semibold text-gray-700 text-lg">
+          <div className="w-full h-max content shadow-md rounded-md bg-white p-4 pb-2 flex flex-col justify-center gap-3">
+            <div className="font-semibold text-gray-700 text-xl">
               Copy the link to share your poll
             </div>
             {/* <input
@@ -52,13 +58,13 @@ const PollCreated = (props: Props) => {
 
             <div
               onClick={(e: any) => copyText(e.target.innerText)}
-              className="tooltip-parent w-full bg-gray-100 hover:bg-gray-200 p-3 rounded-md pr-4 text-gray-600 flex justify-between cursor-pointer"
+              className="tooltip-parent w-full bg-gray-100 hover:bg-gray-200 p-3 rounded-md pr-4 text-gray-600 flex justify-between cursor-pointer text-lg"
             >
               <div>{POLL_URL}</div>
               <CopyIcon className="h-6 w-6 text-red-500" />
             </div>
             <a
-              className="flex justify-end mt-3 text-red-500 font-semibold text-sm"
+              className="flex justify-end mt-3 text-red-500 font-semibold"
               href={POLL_URL}
             >
               <span className="flex items-center gap-1">
